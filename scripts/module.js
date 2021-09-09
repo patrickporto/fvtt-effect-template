3dc7d11f3e47f89b0544b5cd2455693f4c524cfe
Patrick Porto <patrick.s.porto@gmail.com>
2021-09-09 11:35:14 -0300

    feat: add module settings

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,7 @@
-Hooks.once('init', async function() {
+import initModuleSettings from "./settings.js";
 
+Hooks.once('init', async function() {
+    initModuleSettings()
 });
 
 Hooks.once('ready', async function() {
---
+++ scripts/settings.js
@@ -0,0 +1,28 @@
+const initModuleSettings = () => {
+  game.settings.register('effecttemplate', 'targetingOnSelect', {
+    name: game.i18n.localize('EFFECTTEMPLATE.SettingtargetingOnSelect'),
+    hint: game.i18n.localize('EFFECTTEMPLATE.SettingtargetingOnSelectHint'),
+    default: true,
+    scope: 'world',
+    type: Boolean,
+    config: true,
+  })
+  game.settings.register('effecttemplate', 'autoTargeting', {
+    name: game.i18n.localize('EFFECTTEMPLATE.SettingTemplateAutoTargeting'),
+    hint: game.i18n.localize('EFFECTTEMPLATE.SettingTemplateAutoTargetingHint'),
+    default: false,
+    scope: 'world',
+    type: Boolean,
+    config: true,
+  })
+  game.settings.register('effecttemplate', 'autoRemove', {
+    name: game.i18n.localize('EFFECTTEMPLATE.SettingTemplateAutoRemove'),
+    hint: game.i18n.localize('EFFECTTEMPLATE.SettingTemplateAutoRemoveHint'),
+    default: true,
+    scope: 'world',
+    type: Boolean,
+    config: true,
+  })
+}
+
+export default initModuleSettings
